Fixing numerical instability in continuous action spaces.

awr/agent.py
@@ -106,7 +106,7 @@ class Agent(tf.Module):
         state_norm = tf.math.divide_no_nan(
             state - observation_mean, tf.sqrt(observation_var)
         )
-        return state
+        return state_norm
 
     @tf.function
     def initialize(self, env_outputs, agent_outputs):
@@ -142,21 +142,6 @@ class Agent(tf.Module):
     def _continuous(self, hidden):
         loc = self._loc(hidden)
         scale_diag = self._scale_diag(hidden)
-
-        # bijector = tfp.bijectors.Chain(
-        #     [
-        #         tfp.bijectors.Tanh(),
-        #         tfp.bijectors.Affine(shift=loc, scale_diag=scale_diag),
-        #     ]
-        # )
-
-        # base_dist = tfp.distributions.MultivariateNormalDiag(
-        #     loc=tf.zeros_like(loc), scale_diag=tf.ones_like(scale_diag)
-        # )
-        # policy = tfp.distributions.TransformedDistribution(
-        #     distribution=base_dist, bijector=bijector
-        # )
-
         policy = tfp.distributions.Normal(loc=loc, scale=scale_diag)
         return policy
 

awr/algorithm.py
@@ -237,7 +237,7 @@ class Algorithm:
         self._train_value(env_outputs)
         self._train_policy(agent_outputs, env_outputs)
 
-    # @tf.function
+    @tf.function
     def _train_data(self, agent_outputs, env_outputs):
         # Make tensorflow dataset from batch of sampled trajectories.
         dataset = (
@@ -263,7 +263,7 @@ class Algorithm:
             self.buffer.write(flatten_transitions(agent_outputs, env_outputs))
         else:
             self.buffer.write((agent_outputs, env_outputs))
-
+        
         # Make summaries.
         if not self.gcp:
             with self.job.summary_context("train"):
@@ -282,13 +282,13 @@ class Algorithm:
                     agent_outputs, env_outputs = self._collect_transitions(
                         self.explore_strategy, self.params.episodes_train
                     )
+                    episodic_reward = episodic_mean(env_outputs.reward)
 
                 # Update transition buffer with exploration trajectories.
                 self._update_buffer(agent_outputs, env_outputs)
 
                 # Make summaries.
                 with self.job.summary_context("train"):
-                    episodic_reward = episodic_mean(env_outputs.reward)
 
                     tf.summary.scalar(
                         "episodic_rewards/train",