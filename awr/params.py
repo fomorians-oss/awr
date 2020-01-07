40433006c171e414d4a357618afbe9faa92b2f1f
Dan Saunders <djsaunde@umass.edu>
2020-01-07 18:56:29 -0500

    Different default hyperparameters.

--- awr/params.py
+++ awr/params.py
@@ -12,14 +12,14 @@ class HyperParams:
     iterations = attr.ib(default=1000, type=int)  # number of train/eval iterations
     max_size = attr.ib(default=int(5e2), type=int)  # maximum transition buffer size
     max_size_flat = attr.ib(default=int(5e4))
-    num_samples = attr.ib(
-        default=64 * 100, type=int
-    )  # number of transitions to sample
+    num_samples = attr.ib(default=64 * 100, type=int)  # number of transitions to sample
     num_samples_flat = attr.ib(
-        default=256 * 250, type=int
+        default=1024 * 100, type=int
     )  # number of transitions to sample
     batch_size = attr.ib(default=64, type=int)  # dataset batch size each iteration
-    batch_size_flat = attr.ib(default=256, type=int)  # dataset batch size each iteration
+    batch_size_flat = attr.ib(
+        default=1024, type=int
+    )  # dataset batch size each iteration
     steps_init = attr.ib(default=int(1e2), type=int)
     steps_init_flat = attr.ib(default=int(1e3), type=int)  # number of initial episodes
     eval_iters = attr.ib(default=10, type=int)
@@ -29,8 +29,8 @@ class HyperParams:
     clipnorm = attr.ib(default=1.0, type=float)
     value_scale = attr.ib(default=0.5, type=float)
     lambda_ = attr.ib(default=0.95, type=float)
-    beta = attr.ib(default=0.01, type=float)
-    score_max = attr.ib(default=20.0, type=float)
+    beta = attr.ib(default=1.0, type=float)
+    score_max = attr.ib(default=100.0, type=float)
     l2_coef = attr.ib(default=1e-3, type=float)
 
     @staticmethod
